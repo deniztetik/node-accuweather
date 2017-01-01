4226d973fdc63e1e42d8b59dfdb849dd7199d5fa
Deniz Tetik <deniztetik31@gmail.com>
2016-12-31 20:01:45 -0500

    [fix] Correctly modularize index.js so it can be processed as npm module

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,41 +4,43 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 var request = require('request');
 
-var accuweather = function accuweather(API_KEY) {
-  // Gets the AccuWeather-specific location key so that a query can be made to get the weather
+var accuweather = function accuweather() {
+  return function (API_KEY) {
+    // Gets the AccuWeather-specific location key so that a query can be made to get the weather
 
-  var getLocationKey = function getLocationKey(query) {
-    return new Promise(function (resolve, reject) {
-      var params = {
-        url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-        qs: { apikey: API_KEY, q: query }
-      };
-      request(params, function (err, response, body) {
-        if (err) reject(err);
-        resolve(JSON.parse(body)[0].Key);
-      });
-    });
-  };
-
-  var getNowWeatherAndRealFeel = function getNowWeatherAndRealFeel(query) {
-    return new Promise(function (resolve, reject) {
-      getLocationKey(query, API_KEY).then(function (key) {
+    var getLocationKey = function getLocationKey(query) {
+      return new Promise(function (resolve, reject) {
         var params = {
-          url: 'http://dataservice.accuweather.com/currentconditions/v1/' + key,
-          qs: { apikey: API_KEY, details: true }
+          url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+          qs: { apikey: API_KEY, q: query }
         };
         request(params, function (err, response, body) {
           if (err) reject(err);
-          resolve({
-            Temperature: JSON.parse(body)[0].Temperature.Imperial.Value,
-            RealFeel: JSON.parse(body)[0].RealFeelTemperature.Imperial.Value
+          resolve(JSON.parse(body)[0].Key);
+        });
+      });
+    };
+
+    var getNowWeatherAndRealFeel = function getNowWeatherAndRealFeel(query) {
+      return new Promise(function (resolve, reject) {
+        getLocationKey(query, API_KEY).then(function (key) {
+          var params = {
+            url: 'http://dataservice.accuweather.com/currentconditions/v1/' + key,
+            qs: { apikey: API_KEY, details: true }
+          };
+          request(params, function (err, response, body) {
+            if (err) reject(err);
+            resolve({
+              Temperature: JSON.parse(body)[0].Temperature.Imperial.Value,
+              RealFeel: JSON.parse(body)[0].RealFeelTemperature.Imperial.Value
+            });
           });
         });
       });
-    });
-  };
+    };
 
-  return _defineProperty({ getNowWeatherAndRealFeel: getNowWeatherAndRealFeel }, 'getNowWeatherAndRealFeel', getNowWeatherAndRealFeel);
+    return _defineProperty({ getNowWeatherAndRealFeel: getNowWeatherAndRealFeel }, 'getNowWeatherAndRealFeel', getNowWeatherAndRealFeel);
+  };
 };
 
 module.exports = accuweather;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 const request = require('request')
 
-const accuweather = API_KEY => {
-  // Gets the AccuWeather-specific location key so that a query can be made to get the weather
+const accuweather = () => {
+  return (API_KEY) => {
+    // Gets the AccuWeather-specific location key so that a query can be made to get the weather
 
-  const getLocationKey = (query) => new Promise((resolve, reject) => {
-    const params = {
-      url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-      qs: {apikey: API_KEY, q: query}
-    }
-    request(params, (err, response, body) => {
-      if (err) reject(err)
-      resolve(JSON.parse(body)[0].Key)
+    const getLocationKey = (query) => new Promise((resolve, reject) => {
+      const params = {
+        url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+        qs: {apikey: API_KEY, q: query}
+      }
+      request(params, (err, response, body) => {
+        if (err) reject(err)
+        resolve(JSON.parse(body)[0].Key)
+      })
     })
-  })
 
-  const getNowWeatherAndRealFeel = (query) => {
-    return new Promise((resolve, reject) => {
-      getLocationKey(query, API_KEY)
-        .then(key => {
-          const params = {
-            url: 'http://dataservice.accuweather.com/currentconditions/v1/' + key,
-            qs: {apikey: API_KEY, details: true}
-          }
-          request(params, (err, response, body) => {
-            if (err) reject(err)
-            resolve({
-              Temperature: JSON.parse(body)[0].Temperature.Imperial.Value,
-              RealFeel: JSON.parse(body)[0].RealFeelTemperature.Imperial.Value
+    const getNowWeatherAndRealFeel = (query) => {
+      return new Promise((resolve, reject) => {
+        getLocationKey(query, API_KEY)
+          .then(key => {
+            const params = {
+              url: 'http://dataservice.accuweather.com/currentconditions/v1/' + key,
+              qs: {apikey: API_KEY, details: true}
+            }
+            request(params, (err, response, body) => {
+              if (err) reject(err)
+              resolve({
+                Temperature: JSON.parse(body)[0].Temperature.Imperial.Value,
+                RealFeel: JSON.parse(body)[0].RealFeelTemperature.Imperial.Value
+              })
             })
           })
-        })
-    })
-  }
+      })
+    }
 
-  return { getNowWeatherAndRealFeel, getNowWeatherAndRealFeel }
+    return { getNowWeatherAndRealFeel, getNowWeatherAndRealFeel }
+  }
 }
 
 module.exports = accuweather
